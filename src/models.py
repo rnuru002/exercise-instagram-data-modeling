eb7f9de830c3d71c2b8c946c0244df6f2914ab39
rnuru002 <rnuru002@gmail.com>
2022-08-03 18:11:27 +0000

    add nwe

--- src/models.py
+++ src/models.py
@@ -9,20 +9,19 @@ from eralchemy import render_er
 Base = declarative_base()
 
 class Person(Base):
-    __tablename__ = 'person'
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    address = Column(String(350), nullable=False)
 
 class Address(Base):
-    __tablename__ = 'address'
+    __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+    character_id = Column(Integer, primary_key=True)
+    user_id = Column(String(250))
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
